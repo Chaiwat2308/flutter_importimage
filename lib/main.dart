f9dd00f8b19a66ed507be8b1eaefbfe8c8b763fa
Anuchit Chalothorn <anoochit@gmail.com>
2024-06-02 08:20:57 +0700

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,3 @@
-// void main() {
-//   runApp(
-//     const MaterialApp(
-//       home: Scaffold(
-//         body: GradientContainer(
-//           Color.fromARGB(255, 33, 5, 109),
-//           Color.fromARGB(255, 68, 21, 149),
-//         ),
-//       ),
-//     ),
-//   );
-// }
-
 import 'package:flutter/material.dart';
 import 'package:three_project/gradient_container.dart';
 
